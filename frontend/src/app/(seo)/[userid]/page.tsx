dad494d550700b0eeb6bbeda54d1a304924c3ee9
shabinx30 <shabeensharih@gmail.com>
2025-08-18 15:44:48 +0530

    fix: type with generics

--- frontend/src/app/(seo)/[userid]/page.tsx
+++ frontend/src/app/(seo)/[userid]/page.tsx
@@ -2,13 +2,14 @@ import { findUser } from "@/services";
 import { notFound } from "next/navigation";
 import React from "react";
 
-interface ProfilePageProps {
-    params: {
-        userid: string;
-    };
-}
+type PageProps<TParams = {}, TSearchParams = {}> = {
+    params: TParams;
+    searchParams?: TSearchParams;
+};
 
-export default async function Profile({ params }: ProfilePageProps) {
+export default async function Profile({
+    params,
+}: PageProps<{ userid: string }>) {
     const { userid } = params;
     const data = await findUser(userid);
 

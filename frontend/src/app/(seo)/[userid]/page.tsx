50b2bd8b6ab5dc22eea28b25775fa5db05720ea5
shabinx30 <shabeensharih@gmail.com>
2025-08-18 16:02:59 +0530

    fix: added promise for pageProps

--- frontend/src/app/(seo)/[userid]/page.tsx
+++ frontend/src/app/(seo)/[userid]/page.tsx
@@ -2,15 +2,12 @@ import { findUser } from "@/services";
 import { notFound } from "next/navigation";
 import React from "react";
 
-type PageProps<TParams = {}, TSearchParams = {}> = {
-    params: TParams;
-    searchParams?: TSearchParams;
-};
-
 export default async function Profile({
     params,
-}: PageProps<{ userid: string }>) {
-    const { userid } = params;
+}: {
+    params: Promise<{ userid: string }>;
+}) {
+    const userid = (await params).userid;
     const data = await findUser(userid);
 
     if (!data) {
